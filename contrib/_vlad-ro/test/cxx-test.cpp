a11b16e2b5fb4a89ea1b70ea7355f70b70d4ce9f
Vlad Romanenko <vlad-ro@users.noreply.github.com>
2017-10-01 19:12:05 +0100

    Add test cases for C++.

--- contrib/_vlad-ro/test/cxx-test.cpp
+++ contrib/_vlad-ro/test/cxx-test.cpp
@@ -1,16 +1,34 @@
 
-Comment followed by a string:
+// Comment followed by a string is broken:
 
 /*".."      return KW_Range; */
 //"" test
 
-Comment followed by a preprocessor:
+// Comment followed by a preprocessor works:
 
 /*#define */
 //#define
-
 
 "single line string - ok"
 
 "multiline string
 is not valid!?"
+
+// Are all these valid chars?
+'\"'
+'\''
+'''
+'"'
+'\n'
+'\t'
+'\0'
+'\1'
+'\02'
+'\023'
+
+// Are all these valid chars inside a string?
+"foo \' bar"
+"foo \" bar"
+"foo \0 bar"
+"foo \t bar"
+"foo \n bar"
